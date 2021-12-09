bc915f2ede260e628802d83335065b354282563b
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-12-09 12:34:19 +0530

    Initial commit

---
+++ app/src/main/java/com/bhardwaj/messaging/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bhardwaj.messaging
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.bhardwaj.messaging.ui.theme.FirebaseCloudMessagingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
