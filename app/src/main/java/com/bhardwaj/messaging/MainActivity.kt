6b148528a8cd91b2faaeb82abece5a4bab5427c2
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2021-12-18 23:30:57 +0530

    Trying Hands on Channels in Android

--- app/src/main/java/com/bhardwaj/messaging/MainActivity.kt
+++ app/src/main/java/com/bhardwaj/messaging/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -15,9 +16,11 @@ import com.bhardwaj.messaging.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val CHANNEL_ID = "1"
+    private val CHANNEL_ID_1 = "1"
+    private val CHANNEL_ID_2 = "2"
     private lateinit var mContext: Context
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,13 +31,20 @@ class MainActivity : AppCompatActivity() {
         notificationChannel()
 
         binding.btn.setOnClickListener {
+            val numberOfCookies = binding.editCookies.text.toString()
+
             val intent = Intent(mContext, MainActivity2::class.java)
-            val pendingIntent = PendingIntent.getActivity(mContext, 0, intent, 0)
+            intent.putExtra("cookies", numberOfCookies)
+            val pendingIntent = PendingIntent.getActivity(
+                mContext,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+            )
 
             val largeIconBitmap = BitmapFactory.decodeResource(resources, R.drawable.icon_cookies)
 
-            val numberOfCookies = binding.editCookies.text.toString()
-            val builder = NotificationCompat.Builder(mContext, CHANNEL_ID)
+            val builder = NotificationCompat.Builder(mContext, CHANNEL_ID_1)
                 .setSmallIcon(R.drawable.icon_message)
                 .setContentTitle("Cookies")
                 .setContentText("You got $numberOfCookies Cookies")
@@ -46,6 +56,9 @@ class MainActivity : AppCompatActivity() {
                         .bigPicture(largeIconBitmap)
                         .bigLargeIcon(null)
                 )
+                .addAction(R.mipmap.ic_launcher, "Option 1", pendingIntent)
+                .addAction(R.mipmap.ic_launcher, "Option 2", pendingIntent)
+                .setColor(resources.getColor(R.color.purple_700, theme))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build()
 
@@ -56,13 +69,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun notificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "My Channel Name"
+            val name1 = "My Channel Name - 1"
+            val name2 = "My Channel Name - 2"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance)
-            channel.description = "My Channel Description"
+
+            val channel1 = NotificationChannel(CHANNEL_ID_1, name1, importance)
+            channel1.description = "My Channel Description - 1"
+
+            val channel2 = NotificationChannel(CHANNEL_ID_2, name2, importance)
+            channel2.description = "My Channel Description - 2"
 
             val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            manager.createNotificationChannel(channel)
+            manager.createNotificationChannel(channel1)
+            manager.createNotificationChannel(channel2)
         }
     }
 }
--- app/src/main/java/com/bhardwaj/messaging/MainActivity2.kt
+++ app/src/main/java/com/bhardwaj/messaging/MainActivity2.kt
@@ -1,11 +1,20 @@
 package com.bhardwaj.messaging
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var mContext: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        mContext = this@MainActivity2
+        val numberOfCookies = intent.getStringExtra("cookies")
+
+        Toast.makeText(mContext, "$numberOfCookies", Toast.LENGTH_SHORT).show()
     }
 }
